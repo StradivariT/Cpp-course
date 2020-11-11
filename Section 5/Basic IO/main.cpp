3822a27c1f64ad3284d6f2eef3f1d7e198562cf3
fevargas <fernando.vargas@oracle.com>
2020-11-11 14:37:51 -0600

    Add section 5 stuff

---
+++ Section 5/Basic IO/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+int main() {
+    // std::cout << "Hey" << std::endl;
+
+    // std::cout << "Hey";
+    // std::cout << "Bruh" << std::endl;
+
+    // std::cout << "Hey" << std::endl;
+    // std::cout << "Hey" << " bruh" << std::endl;
+    // std::cout << "Hey" << " bruh\n";
+    // std::cout << "Hey\nbruh\nlol\n";
+
+    int num1;
+    int num2;
+    double num3;
+
+    // std::cout << "Enter an integer: ";
+    // std::cin >> num1;
+    // std::cout << "You typed: " << num1 << std::endl;
+
+    // std::cout << "Enter a first integer: ";
+    // std::cin >> num1;
+
+    // std::cout << "Enter a second integer: ";
+    // std::cin >> num2;
+
+    // std::cout << "You entered " << num1 << " and " << num2 << std::endl;
+
+    // std::cout << "Enter 2 integers separated by a space: ";
+    // std::cin >> num1 >> num2;
+    // std::cout << "You entered " << num1 << " and " << num2 << std::endl;
+
+    // std::cout << "Enter a double: ";
+    // std::cin >> num3;
+
+    // std::cout << "You entered: " << num3 << std::endl;
+
+    std::cout << "Enter an integer: ";
+    std::cin >> num1;
+
+    std::cout << "Enter a double: ";
+    std::cin >> num3;
+
+    std::cout << "The integer is: " << num1 << std::endl;
+    std::cout << "The double is: " << num3 << std::endl;
+
+    return 0;
+}
